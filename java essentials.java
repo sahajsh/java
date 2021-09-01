69ce44f0326329e53de006e1e93beb4303cf8f39
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-01 23:08:36 +0530

    Create java essentials.java

---
+++ java essentials.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.next());//it can print sahaj
+        System.out.println(sc.nextInt());//it print integer value 
+        System.out.println(sc.nextLine());//it will print whole string sahaj shukla
+	}
+}
+
