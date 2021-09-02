09d31ec88e73c0b5f69d1a32f602e8fd9c574665
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-02 08:11:27 +0530

    Update java essentials.java

--- java essentials.java
+++ java essentials.java
@@ -17,3 +17,19 @@ class Main
 }
 
 //int rollno=10  10 is literal and rollno is identifier any class ,package name method function is identifier
+//Sum of 2 numbers
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.println(sum);
+    }
+}
