98ac616c1fea4542163f809ece50482c09432e97
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-10 00:28:18 +0530

    Update ass1.java

--- ass1.java
+++ ass1.java
@@ -1,1 +1,19 @@
+//program -1
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner in=new Scanner(System.in);
+        int num=in.nextInt();
+        String check=(num % 2 ==0) ? "even": "odd";
+        System.out.println("the following number is " +check);
+    }
+}
+
 
