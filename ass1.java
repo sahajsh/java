9f8ee0c249d2398258c144fe4bc4483bf8f257d5
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-10 00:30:27 +0530

    Update ass1.java

--- ass1.java
+++ ass1.java
@@ -16,4 +16,24 @@ class Main
     }
 }
 
+//program-2
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner in=new Scanner(System.in);
+        String name=in.nextLine();
+        System.out.println("hello my brother  " + name);
+
+    }
+}
+
+
 
