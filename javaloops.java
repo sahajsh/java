edc5fcdf0c915a9f885c1be48503b1a05b8ee2c9
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-10 15:21:26 +0530

    Create javaloops.java

---
+++ javaloops.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        for(int i=0;i<=number;i++){
+            System.out.println(i);
+        }
+
+        
+    }
+		
+	}
+
+
