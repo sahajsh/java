6b82db54db5d757cb3f6d216a65e733943d90677
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-11 02:01:30 +0530

    Update largest number of 3 numbers programs.java

--- largest number of 3 numbers programs.java
+++ largest number of 3 numbers programs.java
@@ -28,3 +28,25 @@ class Main
 
 //multiple if means dono m correct condition h to dono execute hongy. else if me koi ek hi hoga upr wala incorrect h to neechy wala
 //execute.
+//--------------------------------------by using math function---------------------------------------------------------------------------------------------
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+        Scanner in=new Scanner(System.in);
+        int num1=in.nextInt();
+        int num2=in.nextInt();
+        int num3=in.nextInt();
+        int max=Math.max(num3 ,Math.max(num1,num2));
+        System.out.println(max);
+	}
+}
+
