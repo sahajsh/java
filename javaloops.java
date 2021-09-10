d470f1c06a4b8feb321f3ed01e85e698ec57774e
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-10 16:00:02 +0530

    Update javaloops.java

--- javaloops.java
+++ javaloops.java
@@ -17,5 +17,29 @@ class Main
     }
 		
 	}
+//while loop
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+        public static void main (String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int num=sc.nextInt();
+       int i=1;
+
+       while(i<=5){
+           System.out.println("hello world");
+           i++;
+       }
+        
+    }
+		
+	}
+
+
 
 
