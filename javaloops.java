04e8510a39f3cf0943349c9190fefb07311c8801
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-13 01:35:46 +0530

    Update javaloops.java

--- javaloops.java
+++ javaloops.java
@@ -91,4 +91,33 @@ class Main
 	}
 }
 
+//in if else one condition gonna be run.
+
+//////////////////////////////////////////////////////////////////////else if condition //////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+	int salary=400;
+        if(salary>300){
+            salary=salary+400;//if it is true then it execute rest will not
+        }
+        else if(salary>200) {
+            salary=salary-200;//if first is false then it came to second and it gonna execute. 
+        }
+        else{
+            salary+=salary-100;//if above both the condition is false then last block of code will execute.
+        }
+
+        System.out.println(salary);
+
+	}
+}
+
 //in if else one condition gonna be run 
