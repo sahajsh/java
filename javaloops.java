1997960ee12810e50647048ff76307d2bea01b1c
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-13 01:25:14 +0530

    Update javaloops.java

--- javaloops.java
+++ javaloops.java
@@ -40,6 +40,29 @@ class Main
 		
 	}
 
+//basic concept clearing of if statement->
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+        int salary=400;
+        if(salary>100){
+            salary=salary+200;
+        }
+        if(salary>=600){
+            salary=salary+100;
+        }
 
+        System.out.println(salary);
+
+	}
+}
+
+//700
+//if me both condition will execute.
