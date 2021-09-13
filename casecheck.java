c153b85e1bda900f91a946d1d24cbb650d82465d
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-13 23:58:47 +0530

    Update casecheck.java

--- casecheck.java
+++ casecheck.java
@@ -1,1 +1,20 @@
 
+//Basic properties-->
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().trim().charAt(0);
+		System.out.println(ch);//c input and output is also c
+		
+	}
+}
+
