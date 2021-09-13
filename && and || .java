1e5974e5adfd9736055925a28bbee98fa086b4f3
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-14 02:04:46 +0530

    Create && and || .java

---
+++ && and || .java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		int a=10;
+		int b=20;
+		if(a==10 && b==20){
+			System.out.println("hello");//both true result print.
+		}
+		if(a==10 || b==30){
+			System.out.println("sahaj");//if one is true it print result.
+		}
+
+
+
+
+	}
+}
+	    
+
+
