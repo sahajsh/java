0d450013a35a6f0da2162b355753998c28b2c9df
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-14 00:21:25 +0530

    Update casecheck.java

--- casecheck.java
+++ casecheck.java
@@ -17,4 +17,26 @@ class Main
 		
 	}
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		//char ch=sc.next().trim().charAt(0);
+		//System.out.println(sc.next().trim().charAt(0));
+		String word=sc.nextLine();
+		System.out.println(word.charAt(0));
+
+
+	}
+}
+
 
