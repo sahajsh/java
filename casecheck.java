58ec3661101beb96b381dc6ac1983be9e3efb780
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-14 02:24:18 +0530

    Update casecheck.java

--- casecheck.java
+++ casecheck.java
@@ -33,8 +33,35 @@ class Main
 		//char ch=sc.next().trim().charAt(0);
 		//System.out.println(sc.next().trim().charAt(0));
 		String word=sc.nextLine();
+		
+		
 		System.out.println(word.charAt(0));
 
+
+	}
+}
+
+//////////////////////////////alphabet case check example/////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+	   Scanner sc=new Scanner(System.in);
+	   char ch= sc.next().charAt(0);
+	   
+	   if (ch >='a' && ch<='z'){
+		   System.out.println("lower case");
+	   
+	   }else {
+		   System.out.println("upper case");
+	   }
 
 	}
 }
