04867ee40ae6da2cc627f8734228ee48c03ec22a
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-19 03:06:17 +0530

    Create Parameters(integer function).java

---
+++ functions/Parameters(integer function).java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+		int ans=sum3(20,10);
+        System.out.println(ans);
+	}
+    static int sum3(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+}
+
