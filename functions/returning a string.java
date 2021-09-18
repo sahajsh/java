8238cbe07dd67f5296632eb49b85a95758f5cbf7
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-19 02:45:58 +0530

    Create returning a string.java

---
+++ functions/returning a string.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        String messg=greet();
+        System.out.println(messg);
+        
+	}
+    static String greet(){
+        String greetings="hello how are you";
+        return greetings;
+    }
+
+}
+
