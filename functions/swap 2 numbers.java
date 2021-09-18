45a751aa94c0c3a8c15f357f3a12d87261f9f2ce
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-19 04:11:08 +0530

    Create swap 2 numbers.java

---
+++ functions/swap 2 numbers.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int a=10;
+        int b=30;
+        int temp=b;
+        b=a;
+        a=temp;
+        System.out.println(a+ " " +b);
+	}
+}
+
