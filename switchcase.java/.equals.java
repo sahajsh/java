1110166908f307a2e44b023a8fcc3ca4aad9d1cd
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-18 22:27:59 +0530

    Create .equals.java

---
+++ switchcase.java/.equals.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args)
+	{ 
+      Scanner sc=new Scanner(System.in);
+      String fruit=sc.next();
+      if(fruit.equals("mango")){
+          System.out.println("good fruit");
+      }
+      if( fruit.equals("apple")){
+          System.out.println("tasty fruit");
+      }
+
+	}
+}
+
+
