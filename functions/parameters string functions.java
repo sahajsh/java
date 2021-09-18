52fbfa6a10eca66e11d182b3af62893c3bc6507a
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-09-19 03:31:34 +0530

    Create parameters string functions.java

---
+++ functions/parameters string functions.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+     String greetings=greet("sahaj shukla");
+     System.out.println(greetings);
+	}
+    static String greet(String name){
+        String messg="hello "+ name;
+        return messg;
+    }
+   
+    }
+
+
