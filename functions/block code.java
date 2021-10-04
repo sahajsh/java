8719307fb1a19c01b0569f16edd5e73956ec1bc2
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-04 05:31:05 +0530

    Update block code.java

--- functions/block code.java
+++ functions/block code.java
@@ -8,16 +8,17 @@ class Main
 {
 	public static void main (String[] args) 
 	{
-		int a =87;
+	int a =87;
         int b =23;
        {
-           int a =78;//it gives error we cannot initialise same a value as a is declare abov also we can modify it.
-           a=78;//it is correct.
+          // int a =78;//it gives error we cannot initialise same a value  a  inside block as it is already initialise above in same method we cannot initialise again
+	       //hence we can modify it.
+           a=78;//it is correct,change the value
            //value initialised in block will remain in block
            int c=23;
            sop(c);//it is correct 
        }
-        sop(c); //it gives error we access c outside the block
+        //sop(c); //it gives error we access c outside the block
         
 
 
