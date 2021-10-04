f6f1f62b49ac437c48733920fde6de6ddb05b98d
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-05 03:10:25 +0530

    Update shadowing.java

--- functions/shadowing.java
+++ functions/shadowing.java
@@ -1,4 +1,5 @@
 //shadowing concept->
+//shadowing concept->
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -6,8 +7,17 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Main
 {
-    static int x= 9;//static is object independent we cannot use object dependent things in object independent things.
+    static int x= 9; upper level //this is shadwoed lower level is overriding with upper level
+    //static is object independent we cannot use object dependent things in object independent things.
 	public static void main (String[] args) {
         System.out.println(x);//9
+        int x =7;//lower level
+        System.out.println(x);//7
+        fun();
 	}
+    static void fun(){
+        System.out.println(x);//9
+    }
 } 
+
+//in this scope is overlapping uppper level is shadwoed lower level is overriding with upper level.
