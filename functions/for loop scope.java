e64a1b18bfd4ff41a0342d363a82000d85586233
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-04 08:42:39 +0530

    Create for loop scope.java

---
+++ functions/for loop scope.java
@@ -0,0 +1,26 @@
+// Block code 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+ public static void main(String[] args){
+     for (int i=0;i<8;i++){
+         System.out.println(i);
+     }
+     // System.out.println(i); we cannot access i outside the scope of for loop it gives error 
+
+ }  
+}
+
+output->
+0
+1
+2
+3
+4
+5
+6
+7
