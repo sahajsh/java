9ca125e231fdaa9752c6b0e1491ddc62b1dbb3dd
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-05 11:57:43 +0530

    Create varargs program.java

---
+++ functions/varargs program.java
@@ -0,0 +1,18 @@
+//variable length arguments ->
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args)
+	{
+		fun(3,4,5,6,746);
+
+	}
+    static void fun(int...var){
+        System.out.println(Arrays.toString(var));
+    }
+}
+
