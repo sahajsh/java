99df801e8b527f76133740454d5beb2cef2213d8
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-05 12:00:08 +0530

    Update varargs program.java

--- functions/varargs program.java
+++ functions/varargs program.java
@@ -16,3 +16,22 @@ class Main
     }
 }
 
+//EXAMPLE -2
+	//variable length arguments ->
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args)
+	{
+		fun("Sahaj","Shukla","Btech C.S.E");
+
+	}
+    static void fun(String...var){
+        System.out.println(Arrays.toString(var));
+    }
+}
+
