769cd33fbc1338f3413ef0c3e7c22235cdde2d79
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-07 12:01:04 +0530

    Update prime number.java

--- programs/prime number.java
+++ programs/prime number.java
@@ -1,1 +1,36 @@
 
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i=2; i*i<=n; i++){
+            if (n%i == 0){
+               count++;
+               break;
+            }
+            
+        }
+        if (count == 0){
+                System.out.println("prime");
+            }
+            else{
+                System.out.println("not prime");
+            }
+
+
+
+
+
+
+	}
+}
+
