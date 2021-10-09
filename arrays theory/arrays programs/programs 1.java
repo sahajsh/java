32226e2f7b37ddb5afea165c7656f14682c53604
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-10 03:10:44 +0530

    Update programs 1.java

--- arrays theory/arrays programs/programs 1.java
+++ arrays theory/arrays programs/programs 1.java
@@ -18,3 +18,5 @@ class Main
 }
 //int datatype represents wt is the datatype stored inside the array and it should be the same.
 //rons(reference variable) pointing to array object contains type integer in the above question. 
+int ros[];//declaration of array . ros is getting defined in stack and it is not pointing to anything
+ros =new int[4];//memory creation happens here pointing to object. object is being created in heap memory.
