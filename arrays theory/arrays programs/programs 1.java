22f7ae453b8f5c33edc1a16fc1d2cb6cdb52dcb2
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-10 02:48:46 +0530

    Update programs 1.java

--- arrays theory/arrays programs/programs 1.java
+++ arrays theory/arrays programs/programs 1.java
@@ -1,1 +1,19 @@
 
+//Syntax of arrays declaration and initialisation ->
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+		int[] rollnos = new int[7];//declaration
+        or 
+
+        int[] rollnos = {2,3,4,2,1,23,33};//initialise
+	}
+}
+
