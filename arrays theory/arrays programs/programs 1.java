a5bdd28dc00a0853e6c37c70effc3a772aa92737
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-10 02:49:57 +0530

    Update programs 1.java

--- arrays theory/arrays programs/programs 1.java
+++ arrays theory/arrays programs/programs 1.java
@@ -10,10 +10,10 @@ class Main
 {
 	public static void main (String[] args) 
 	{
-		int[] rollnos = new int[7];//declaration
+	int[] rollnos = new int[7];//declaration
         or 
 
         int[] rollnos = {2,3,4,2,1,23,33};//initialise
 	}
 }
-
+//int datatype represents wt is the datatype stored inside the array and it should be the same.
