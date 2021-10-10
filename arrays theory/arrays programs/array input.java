156551f3e11c31f6da6fc9c45fee6aee28b2d8db
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-10 08:16:32 +0530

    Create array input.java

---
+++ arrays theory/arrays programs/array input.java
@@ -0,0 +1,23 @@
+//array input 
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+		
+   int[] arr = new int[5];
+   arr[0]=1;
+   arr[1]=2;
+   arr[2]=3;
+   arr[3]=4;
+   arr[4]=5;
+   //[1,2,3,4,5]
+       System.out.println(arr[4]);//4
+	}
+}
+
