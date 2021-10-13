11f906e781819a63e3057ef89254c56e8715db0f
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-13 12:13:25 +0530

    Update declaration of 2 d array .java

--- arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
+++ arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
@@ -24,3 +24,40 @@ arr[2][0] = {4}
 /////////////////////////////////////////////    input of 2 d array  ->//////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//2d array input and output->
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int[][] arr = new int[2][3];
+      System.out.println(arr.length);// no. of rows = 2
+      for(int i =0;i<arr.length;i++){
+          for(int j=0;j<arr[i].length;j++){
+              arr[i][j] = sc.nextInt();
+          }
+      }
+      
+      
+      for(int i =0;i<arr.length;i++){
+          for(int j=0;j<arr[i].length;j++){
+              System.out.print(arr[i][j] + " ");
+          }
+          System.out.println();
+
+      }
+   
+   
+
+    }
+}
+
+output-
+2
+1 2 3 
+4 5 6 
