9915013dda3c0ef2f62a465fffaa30a924b0fe74
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-13 19:06:50 +0530

    Update declaration of 2 d array .java

--- arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
+++ arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
@@ -61,3 +61,28 @@ output-
 2
 1 2 3 
 4 5 6 
+/////////////////////////////////////////////input /output of 2 d array using array.tostring()///////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) 
+	{   Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[3][2];
+        for(int i =0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j] = sc.nextInt();
+            }
+
+        }
+        for(int i =0;i<arr.length;i++){
+           System.out.println(Arrays.toString(arr[i]));
+        }
+	}
+}
+
+////////////////////////////////////////////////////using enhance for loop //////////////////////////////////////////////
