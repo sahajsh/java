31358d970ddb7dff0a8b707b4cc782e3d733c962
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-13 05:49:10 +0530

    Create declaration of 2 d array .java

---
+++ arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
@@ -0,0 +1,21 @@
+//2d array declaration and initialisation->
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	public static void main (String[] args) {
+        int[][] arr = new int[3][];
+        //or
+        int[][] arr2 = {
+                       {2,3,4},
+                       {3,2},
+                       {4,3}
+
+                       };
+		
+	}
+}
+
