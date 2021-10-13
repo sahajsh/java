bdcafa27c92a521740527c9e249eb1ffda5138d2
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-13 19:44:34 +0530

    Update declaration of 2 d array .java

--- arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
+++ arrays theory/Arrays Basic/creating 2 d array/declaration of 2 d array .java
@@ -86,3 +86,28 @@ class Main
 }
 
 ////////////////////////////////////////////////////using enhance for loop //////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+            arr[i][j] = sc.nextInt();
+        }
+        }
+        for(int[] a : arr){
+            System.out.println(Arrays.toString(a));
+        }
+	}
+}
+
+
