f3239cf5027efa6b65cba69e4d91207215414b96
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-15 06:17:22 +0530

    Update input arraylist.java

--- arrays theory/Arrays Basic/Arraylist/input arraylist.java
+++ arrays theory/Arrays Basic/Arraylist/input arraylist.java
@@ -1,1 +1,25 @@
 
+//input of arraylist
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+            ArrayList<Integer> list = new ArrayList<>(5);
+            for(int i =0; i<5; i++){
+                list.add(sc.nextInt());
+            }
+            
+                System.out.println(list);//list index will not work here get item at any index// [1, 2, 3, 4, 5]
+            for(int i =0;i<5;i++){
+                System.out.print(list.get(i));// 1 2 3 4 5 
+            }
+        
+          
+	}
+}
+
