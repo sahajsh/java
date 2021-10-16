c361a2ce2117d1f1873b1b9f8f589c4bf38f226c
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-16 12:05:16 +0530

    Update MAX value find in array.java

--- arrays theory/arrays programs/MAX value find in array.java
+++ arrays theory/arrays programs/MAX value find in array.java
@@ -23,4 +23,40 @@ class Main
       return max_value;
     }
 }
+//edge cases when array is being null ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+	   int[] arr = {1,2,77,466};
+      System.out.println(maxval(arr,1,3));
+	}
+   
+    static int maxval(int[] arr,int start,int end){
+        int max_value = arr[0];
+        if(arr.length==0){
+            return -1;
+        }
+         if(end>start){
+        return -1;
+    }
+    if(arr==null){
+        return -1;
+        }
+        for(int i =start;i<=end;i++){
+            if(arr[i] > max_value){
+                max_value= arr[i];
+            }
+        }
+        return max_value;
+
+    }
+}
+
 
