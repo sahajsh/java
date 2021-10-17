c67feea4dfc680912d5d2d77c2bc46881efc16ea
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-17 12:55:07 +0530

    Update linear search code.java

--- linear search/programs/linear search code.java
+++ linear search/programs/linear search code.java
@@ -33,3 +33,40 @@ class Main
 }
 output is 4 
 
+////////////////////////////////////////////////////////if we want to return element in linear search ------------------------------>
+	
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+	// we return element if it is found in array using linear search 
+        int[] nums ={1,2,3,4,5};
+        int target =3;//output is 4 
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+	}
+  static int linearSearch(int[] arr, int target){
+      if(arr.length== 0){
+          return -1;// doesn't exist 
+      }
+      //run a for loop  
+      for(int element: arr){
+          
+         
+          if(element == target){
+              return element;// this value will return to the function call
+          }
+      }
+// this line will execute if none of the return statements is executed so target is not found . element doesn't exist it return -1
+   return -1;
+
+  }
+}
+
