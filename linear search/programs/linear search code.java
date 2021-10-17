71b387169c807adc6c61b6edad615c7abb15af89
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-17 08:50:59 +0530

    Update linear search code.java

--- linear search/programs/linear search code.java
+++ linear search/programs/linear search code.java
@@ -3,12 +3,12 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+
 class Main
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// we are going to search in an array : if item is found then we gonna written index 
+	// we are going to search in an array : if item is found then we gonna written index 
         //otherwise if element is not found we gonna return -1
         int[] nums ={1,2,3,4,5};
         int target =6;
